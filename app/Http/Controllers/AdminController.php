151f72484e2d4fc0111bf5a46cbb790c9807e9d9
SayorHasan <md.sayor.hasan@g.bracu.ac.bd>
2025-08-22 20:04:42 +0600

    created new category system for the admin

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -104,4 +104,34 @@ class AdminController extends Controller
         $categories = Category::orderBy('id','DESC')->paginate(10);
         return view('admin.categories',compact('categories'));
     }
+    public function category_add(){
+        return view('admin.category-add');
+    }
+    public function category_store(Request $request){
+        $request->validate([
+            'name' => 'required',
+            'slug' => 'required|unique:brands,slug',
+            'image' => 'mimes:png,jpg,jpeg|max:2048'
+
+        ]);
+        $category = new Category();
+        $category->name = $request->name;
+        $category->slug = Str::slug($request->name);
+        $image = $request->file('image');
+        $file_extension =  $request->file('image')->extension();
+        $file_name = Carbon::now()->timestamp.'.'.$file_extension;
+        $this->GenerateCategoryThumbailsImage($image,$file_name);
+        $category->image = $file_name;
+        $category->save();
+        return redirect()->route('admin.categories')->with('status','Category has been added successfully');
+    }
+
+    public function GenerateCategoryThumbailsImage($image,$imageName){
+        $destinationPath = public_path('uploads/categories');
+        $img = Image::read($image->path());
+        $img->cover(124,124,"top");
+        $img->resize(124,124,function($constraint){
+            $constraint->aspectRatio();
+        })->save($destinationPath.'/'.$imageName);
+    }
 }
